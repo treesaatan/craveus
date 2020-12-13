2118eaac4750d278daabfc8197ac8b8a68c175cb
tes <tressa0923@gmail.com>
2020-12-12 17:56:58 -0800

    Updates

---
+++ precache-manifest.745ddc4c352f57c4b2f25ae4201e3259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9db3537d2243aa826aff6117283d0c",
+    "url": "/craveus/index.html"
+  },
+  {
+    "revision": "4e72323f83ad79816515",
+    "url": "/craveus/static/css/main.025b8b47.chunk.css"
+  },
+  {
+    "revision": "aec531b41794244e8c81",
+    "url": "/craveus/static/js/2.081ca528.chunk.js"
+  },
+  {
+    "revision": "4e72323f83ad79816515",
+    "url": "/craveus/static/js/main.a63920da.chunk.js"
+  },
+  {
+    "revision": "a476eb49c524cedd75cf",
+    "url": "/craveus/static/js/runtime~main.7887b803.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/craveus/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/craveus/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
